d7bce3285139cb947ff690820352aa402ef6605a
beMaddz <madhu05ghosh@gmail.com>
2016-12-02 15:29:00 +0530

    WIP: CRUDops: create, read

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongo = require('mongodb').MongoClient;
+const pug = require('pug');
 const app = express();
+
+app.set('view engine', 'pug');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  res.sendFile('/home/madhu/javaScript/ToDo-List' + '/index.html');
-});
-app.post('/task', function(req, res) {
-  console.log(req.body);
+  mongo.connect('mongodb://localhost:27017/test', function(err, db) {
+    db.collection('ToDo').find().toArray(function(err, result) {
+      if(err)
+      console.log(err);
+      res.render('index', {todo: result});
+    });
+  });
 });
 
-app.listen(3000, function() {
-  console.log("listening to 3000");
+app.post('/task', function(req, res) {
+  var list = req.body;
+  mongo.connect('mongodb://localhost:27017/test', function(err, db) {
+    db.collection('ToDo').insert(list, function(err, result) {
+      console.log('item inserted');
+    });
+  });
+  res.redirect('/');
+});
+
+app.listen(3000, function () {
+    console.log("listening to 3000");
 });
